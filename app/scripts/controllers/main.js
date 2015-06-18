16b7b4f02bab88801ebab7363f7bccff96fc86bc
oroborusm <juan.bustamante.dg@gmail.com>
2015-06-18 05:39:01 -0300

    imagen en dropbox

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,13 +13,10 @@ angular.module('papaseguroApp')
     $scope.slides = [
       {
         titulo: 'Primera Imagen',
-        imagen: '/images/foto1.png'
+        imagen: 'https://photos-6.dropbox.com/t/2/AACuWWUHPBKtOpPjuNzTGlHy1apR2v7vRfybXw5w1Fcphg/12/133525864/png/32x32/1/_/1/2/foto1.png/COji1T8gASACKAEoAg/509DHPin3AnRkeBm3C9okKE2Oho812F5a_M4khENOsY?size=1024x768&size_mode=2'
       },{
         titulo: 'Segunda Imagen',
-        imagen: '/images/foto2.png'
-      },{
-        titulo: 'Tercera Imagen',
-        imagen: '/images/foto3.png'
+        imagen: 'https://photos-2.dropbox.com/t/2/AABnvG3eC07kZi-e_GfxiHQflYk_mHVfP1Fya9HGtyZ77A/12/133525864/png/32x32/1/_/1/2/foto2.png/COji1T8gASACKAEoAg/d8U6XdwFQCB27V21-jTE1o38162dQWBZmpKDVPFjC0c?size=1024x768&size_mode=2'
       }
     ];
   });
